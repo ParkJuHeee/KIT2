0003477389e2bf1a41c101e6483f51ac92816503
ParkJuHeee <wngmlgksk2@naver.com>
2018-11-16 20:56:12 +0900

    네비게이션 바 액티비티

--- app/src/main/java/com/example/kit/MainActivity.java
+++ app/src/main/java/com/example/kit/MainActivity.java
@@ -1,63 +1,62 @@
 package com.example.kit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.example.kit.Adapter.FragementAdapter;
 
+/*네비게이션 바*/
 public class MainActivity extends AppCompatActivity {
 
-    android.support.v7.widget.Toolbar mToolbar;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        /* toolbar 설정 */
-        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
 
-        getSupportActionBar().setTitle("");
-
-        FragmentPagerAdapter mFragmentPagerAdapter = new FragementAdapter(getSupportFragmentManager());
-        ViewPager mViewPager =  (ViewPager)findViewById(R.id.viewpager);
-        mViewPager.setAdapter(mFragmentPagerAdapter);
-
-        /* tab 설정 */
-        TabLayout mTabLayout = (TabLayout)findViewById(R.id.tab);
-        mTabLayout.setupWithViewPager(mViewPager);
-
-    }
-
-    /* toolbar 메뉴 버튼 추가 */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return  true;
-    }
-
-    /* toolbar 클릭시 event */
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_bar:
-                Toast.makeText(getApplicationContext(),"메뉴 버튼",Toast.LENGTH_LONG).show();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //TODO: 메뉴 선택할때 어떤 창으로 이동할지 (영상 참고)
                 return true;
+            }
+        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-            default:
-                Toast.makeText(getApplicationContext(),"나머지 버튼",Toast.LENGTH_LONG).show();
-                return super.onOptionsItemSelected(item);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containter, new newsKoreanFragment()).commit(); //TODO: 첫화면 설정 MainAcitivy 프래그먼트로 바꿔서 교체하기
+            navigationView.setCheckedItem(R.id.nav_alarm_news); //TODO: 첫화면인데 처음에 어떤 메뉴가 클릭되어있을지 없애도 됨
+        }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
